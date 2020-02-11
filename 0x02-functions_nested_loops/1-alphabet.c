cabab37ec5db93e430f55363f12365dc47a2c8f7
cristiand187 <cristiand187@hotmail.com>
2020-02-11 13:26:33 -0500

    Functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+	_putchar(c);
+
+_putchar('\n');
+return(0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,7 +2,8 @@
 #define holberton
 #include <stdio.h>
 
-
 int _putchar(char Text);
+
+int print_alphabet(void);
 
 #endif
