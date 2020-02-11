2f862d99705e0e8a6a1cf1a9592937e2fc23546d
cristiand187 <cristiand187@hotmail.com>
2020-02-11 14:15:58 -0500

    Functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,11 @@
 /**
  * print_alphabet_x10 - Entry point
  *
- * Return: Always 0 (Success)
+ * 
  */
 void print_alphabet_x10(void)
 {
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < 10; ++i)
 {
 char c;
 for (c = 'a'; c <= 'z'; ++c)
