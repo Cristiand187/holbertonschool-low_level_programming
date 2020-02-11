8d0601540cbdf6b4eef2a31f4db1ddcd62414132
cristiand187 <cristiand187@hotmail.com>
2020-02-11 13:48:12 -0500

    Functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+for (int i = 0; i < 10; i++)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+	_putchar(c);
+
+_putchar('\n');
+}
+return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -5,4 +5,6 @@ int _putchar(char Text);
 
 int print_alphabet(void);
 
+int print_alphabet_x10(void);
+
 #endif
