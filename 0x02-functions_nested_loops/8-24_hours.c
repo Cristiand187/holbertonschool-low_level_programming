3ed123d163e3d09d48665efc81ec4d3f7b61d209
cristiand187 <cristiand187@hotmail.com>
2020-02-11 22:32:25 -0500

    Debuggins

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Entry point n
+ *
+ *
+ * Return: Always n.
+ */
+void jack_bauer(void)
+{
+	char a = '0', b = '0', c = '0', d = '0';
+
+	for (a = '0'; a < '3'; a++)
+		{
+			for (b = '0'; b < '4'; b++)
+				{
+				for (c = '0'; c < '6'; c++)
+				{
+				for (d = '0'; d <= '9'; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+}
+}
+}
+		}
+}
