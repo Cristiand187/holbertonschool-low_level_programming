7c6bb2da59805dd20eb75cfb2c0ff535e97688af
cristiand187 <cristiand187@hotmail.com>
2020-02-13 15:00:09 -0500

    Function print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_square - xxx
+ *
+ * @n: xx
+ *
+ * Return: Description of the returned value
+ */
+
+void print_square(int n)
+{
+	int a = 0, b = 0, c = 35;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < n; b++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
