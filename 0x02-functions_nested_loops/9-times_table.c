3c3c01e10776313dadbecb08e86745abb563952f
cristiand187 <cristiand187@hotmail.com>
2020-02-12 21:59:38 -0500

    Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,29 +8,31 @@
  */
 void times_table(void)
 {
-	int a = 47, b = 47;
-	
+int a = 0, b = 0, c = 0;
 
-	for (a = 47; a < 57; a++)
-		{
-		        	for (b = 0; b < 9; b++)
-				{
-
-					_putchar(a);
-					_putchar(',');
-				       _putchar(' ');
-				       
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c < 10)
+{
+_putchar(' ');
+_putchar(c + '0');
+}
+else
+{
+_putchar(c / 10 + '0');
+_putchar(c % 10 + '0');
 
 }
-			for (b = 0; b < 9; b++)
-				{
-
-					_putchar(b);
-					_putchar(',');
-				       _putchar(' ');
-				       
+if (b != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
 
 }
-				_putchar('\n');
-		}
+_putchar('\n');
+}
 }
