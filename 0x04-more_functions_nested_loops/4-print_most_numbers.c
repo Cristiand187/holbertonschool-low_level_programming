1e220020eb1af844a33dea50db7eab552cec66d9
cristiand187 <cristiand187@hotmail.com>
2020-02-13 12:03:05 -0500

    Function print number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - xxx
+ *
+ * Return: Description of the returned value
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+		{
+			if (a != 2 && a != 4)
+				_putchar(a + '0');
+		}
+	_putchar('\n');
+}
