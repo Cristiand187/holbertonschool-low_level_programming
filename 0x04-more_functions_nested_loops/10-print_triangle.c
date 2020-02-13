955a083a9f984985beaa2722352cb2aab6dd1365
cristiand187 <cristiand187@hotmail.com>
2020-02-13 16:37:19 -0500

    Function print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - xxx
+ *
+ * @n: xx
+ *
+ * Return: Description of the returned value
+ */
+
+void print_triangle(int n)
+{
+	int a = 0, b = n, c = 35;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < n; b++)
+		{
+			if (b >= (n - a - 1))
+				_putchar(c);
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
