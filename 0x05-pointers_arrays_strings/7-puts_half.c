9e22d0dc66cb762562fa131901e04b76f29a94ca
Cristiand187 <Cristiand187@hotmail.com>
2020-02-18 15:38:31 -0500

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "holberton.h"
+
+/**
+ * puts_half - xxx
+ *
+ * @str: yy
+ * Return: Description of the returned value
+ */
+
+
+void puts_half(char *str)
+{
+
+	int len = 0, l = 0, ini = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if ((l % 2) == 0)
+		ini = len / 2;
+	else
+		ini = (len - 1) / 2;
+
+	for  (l = ini; l <= len; l++)
+	{
+		_putchar(str[l]);
+	}
+	_putchar('\n');
+}
