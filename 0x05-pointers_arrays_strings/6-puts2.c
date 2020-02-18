4e6da2ddbc737669ee4fb8b61fa4f91570a196a2
Cristiand187 <Cristiand187@hotmail.com>
2020-02-18 15:28:17 -0500

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "holberton.h"
+
+/**
+ * puts2 - xxx
+ *
+ * @str: yy
+ * Return: Description of the returned value
+ */
+
+
+void puts2(char *str)
+{
+
+	int len = 0, l = 0;
+
+	while (str[len] != '\n')
+		len++;
+
+	for (l = 0; l < len; l++)
+	{
+		if ((l % 2) == 0)
+			_putchar(str[l]);
+	}
+	_putchar('\n');
+}
