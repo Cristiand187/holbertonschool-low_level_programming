767a38c269483366604cf60d6978c44504da939a
Cristiand187 <Cristiand187@hotmail.com>
2020-02-18 19:24:18 -0500

    pointers and arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -19,10 +19,8 @@ char *_strcpy(char *dest, char *src)
 	while (src[n] != '\0')
 		n++;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i <= n; i++)
 		dest[i] = src[i];
-
-	dest[i + 1] = '\0';
 
 	return (dest);
 
