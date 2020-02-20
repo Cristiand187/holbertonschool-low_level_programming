b80bc5a63536e1c30aca38c34b571310dfee2dc1
Cristiand187 <Cristiand187@hotmail.com>
2020-02-20 12:50:49 -0500

    Pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncat - This function is concate two string
+ *
+ * @dest: Arrays destination the two string/arrays
+ * @src: This second string/array
+ * @n: It is Number the byts for copy
+ * returns
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c1 = 0, c2 = 0, i = 0;
+
+	while (dest[c1] != '\0')
+		c1++;
+
+	while (src[c2] != '\0')
+		c2++;
+
+	int ini = c1;
+
+	if (c2 >= n)
+		c2 = n;
+
+	for (i = 0; i < c2; i++)
+	{
+		dest[ini + i] = src[i];
+	}
+
+	/* printf("%s\n", dest);*/
+
+	char *p = dest;
+
+	return (p);
+}
