cc39e78b9cef11d47a346e831d3f4b98d43a15ca
Cristiand187 <Cristiand187@hotmail.com>
2020-02-20 13:01:19 -0500

    Pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - This function is concate two string
+ *
+ * @dest: Arrays destination the two string/arrays
+ * @src: This second string/array
+ * @n: It is Number the byts for copy
+ *
+ * returns
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	char *p = dest;
+
+	return (p);
+}
