10455414e311553a3c928430ff5fe5b4f96435f1
Cristiand187 <Cristiand187@hotmail.com>
2020-02-20 22:35:01 -0500

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,15 @@ int _strcmp(char *s1, char *s2)
 
 	int i = 0;
 
-	i = *s1 - *s2;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] >  s2[i])
+			return (s1[i] - s2[i]);
 
-	return (i);
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+
+		i++;
+	}
+	return (0);
 }
