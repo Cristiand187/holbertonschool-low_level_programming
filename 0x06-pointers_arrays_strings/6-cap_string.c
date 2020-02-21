15ab09da5a792c0b3e24e0e8febf9fd6dba03395
Cristiand187 <Cristiand187@hotmail.com>
2020-02-20 20:46:17 -0500

    Pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * cap_string - This function is concate two string
+ *
+ * @a: Arrays destination the two string/arrays
+ *
+ * Return: Description of the returned value
+ */
+
+char *cap_string(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (((a[i] >= 'a' && a[i] <= 'z')) && a[i - 1] == ' ')
+			a[i] = a[i] - 32;
+
+		i++;
+	}
+
+	return (a);
+}
