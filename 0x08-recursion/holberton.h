797f8428f404053d160abbef26975c3185397f13
Cristiand187 <Cristiand187@hotmail.com>
2020-02-26 13:33:26 -0500

    Arrays

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char Text);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+#endif /* HOLBERTON_H */
