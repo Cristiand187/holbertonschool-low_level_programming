35fc024d5541ec6a6accc436d8e861d11ab9724c
Cristiand187 <Cristiand187@hotmail.com>
2020-02-27 21:35:23 -0500

    Recursion Palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
 
-
 int function_aux(char *s, int i, int len);
-int _strlen_recursion_(char *s);
+
 
 /**
- * _strlen_recursion_ - This function is the print string pointer reverse.
+ * _strlen_recursion - This function is the print string pointer reverse.
  *
  * @s: String to printer
  * Return: xxx
  */
 
-int _strlen_recursion_(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*(s + 0) != '\0')
 	{
 		s++;
-		return (1 + _strlen_recursion_(s));
+		return (1 + _strlen_recursion(s));
 	}
 	else
 		return (0);
@@ -37,7 +36,7 @@ int function_aux(char *s, int i, int len)
 	if (i >= len)
 		return (1);
 	if (s[i] == s[len])
-		return (function_aux(s, i++, len--));
+		return (function_aux(s, i + 1, len - 1));
 	else
 		return (0);
 }
@@ -52,7 +51,7 @@ int function_aux(char *s, int i, int len)
 
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion_(s);
+	int len = _strlen_recursion(s);
 
 	return (function_aux(s, 0, len - 1));
 }
