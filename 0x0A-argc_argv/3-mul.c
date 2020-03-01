c3bd90cbc03e072f632bc67555b8b3bf83aafb36
Cristiand187 <Cristiand187@hotmail.com>
2020-03-01 12:49:34 -0500

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This function print number the arguments
+ * @argc: The number of arguments
+ * @argv: Each string is one of the arguments that was passed to the program
+ * Return: value 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
