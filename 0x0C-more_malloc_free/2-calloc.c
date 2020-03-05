e516b4589ada7ce4414821ac36f38b6c92b67582
Cristiand187 <Cristiand187@gmail.com>
2020-03-05 19:35:09 +0000

    More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char **array;
+	unsigned int i = 0;
+	char *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -22,6 +23,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (array == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		array[i] = '\0';
 
 	return (array);
 }
