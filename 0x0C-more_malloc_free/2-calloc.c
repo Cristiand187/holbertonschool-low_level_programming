db9b6d4bd9a68deb5267a24c01e3e4557adeb370
Cristiand187 <Cristiand187@gmail.com>
2020-03-05 18:42:33 +0000

    More malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _calloc - This function create a arrays
+ *
+ * @nmemb: string for content the arrays
+ * @size: string s2 content the arrays
+ * Return: pointer or null
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char **array;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	return (array);
+}
