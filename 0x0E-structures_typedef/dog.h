319ba08093090dbec2444f4c71ccb6b25e1a3190
Cristiand187 <Cristiand187@gmail.com>
2020-03-09 19:57:51 +0000

    0x0E. C - Structures, typedef - Tasks 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* HOLBERTON_H */
