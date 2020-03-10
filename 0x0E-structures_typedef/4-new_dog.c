feb576e489f626fb50b4090a79ce295b2a64339a
Cristiand187 <Cristiand187@hotmail.com>
2020-03-09 21:20:44 -0500

    0x0E. C - Structures, typedef - Tasks 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,6 +8,7 @@
  * @name: string s2 content the arrays
  * @age: pointer or null
  * @owner: xx
+ * Return: pointer struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -20,7 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	(*d).name = name;
 	(*d).age = age;
-        (*d).owner = owner;
+	(*d).owner = owner;
 
 	return (d);
 }
