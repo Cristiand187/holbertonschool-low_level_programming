9ce59feb40ea3e77e409941295d7fa0591df2224
Cristiand187 <Cristiand187@hotmail.com>
2020-03-09 21:14:12 -0500

    0x0E. C - Structures, typedef - Tasks 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - This function create a arrays
+ *
+ * @name: string s2 content the arrays
+ * @age: pointer or null
+ * @owner: xx
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+		return (NULL);
+
+	(*d).name = name;
+	(*d).age = age;
+        (*d).owner = owner;
+
+	return (d);
+}
