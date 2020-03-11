43bebb86e5a9c81129901443bab7d7b7280f3ffc
Cristiand187 <Cristiand187@hotmail.com>
2020-03-11 07:43:30 -0500

    0x0F. C - Function pointers - Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - funtion pointer print
+ *
+ * @name: xx
+ * @f: xx
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
