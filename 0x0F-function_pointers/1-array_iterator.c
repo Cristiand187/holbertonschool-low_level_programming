226ed4a635db48636ef6f74d2c1a68cafc0048c4
Cristiand187 <Cristiand187@gmail.com>
2020-03-11 14:29:24 +0000

    0x0F. C - Function pointers - task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - funtion pointer printer int decimal and hexadecimal
+ *
+ * @array: number to printer
+ * @size: size the array
+ * @action: array to punters the functions
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+
+		(*action)(array[i]);
+	}
+}
