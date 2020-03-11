1565693600ad7c63431d1c9d7c7ec3331fbd7f21
Cristiand187 <Cristiand187@gmail.com>
2020-03-11 21:14:09 +0000

    0x0F. C - Function pointers - task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "3-calc.h"
+
+/**
+ * op_add - check if a number is equal to 98
+ * @a: the integer to check
+ * @b: xx
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - check if a number is equal to 98
+ * @a: the integer to check
+ * @b: xx
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - check if a number is equal to 98
+ * @a: the integer to check
+ * @b: xx
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - check if a number is equal to 98
+ * @a: the integer to check
+ * @b: xx
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - check if a number is equal to 98
+ * @a: the integer to check
+ * @b: xx
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
