da80a30e6bf5b931807d89c934e84e2cd74d846a
Cristiand187 <Cristiand187@gmail.com>
2020-03-11 18:59:08 +0000

    0x0F. C - Function pointers - task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,15 +18,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+
+	if (array != NULL && cmp != NULL)
 	{
+		for (i = 0; i < size; i++)
+		{
 
-		if ((*cmp)(array[i]))
-			break;
+			if ((*cmp)(array[i]))
+				break;
+		}
+
+		if (i == (size - 1))
+			return (-1);
+
+		return (i);
 	}
-
-	if (i == (size - 1))
-		return (-1);
-
-	return (i);
 }
