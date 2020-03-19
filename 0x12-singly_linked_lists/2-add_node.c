65bdf67c99a33d56f48bfb4339dda4f3e5d42026
cristiand187 <cristiand187@hotmail.com>
2020-03-19 18:36:05 -0500

    0x12. C - Singly linked lists - task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -60,7 +60,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	int len = 0;
 
-	if (str == NULL)
+	if (str == NULL || head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(list_t));
