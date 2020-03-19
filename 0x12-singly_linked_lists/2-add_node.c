392dcf518b2143928f395c83a7cdb427e200e6d5
cristiand187 <cristiand187@hotmail.com>
2020-03-19 18:11:22 -0500

    0x12. C - Singly linked lists - task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -78,10 +78,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->str =  strdup(str);
+	new->str = strdup(str);
 	new->len = len;
 	new->next = *head;
 	*head = new;
 
-	return (*head);
+	return (new);
 }
