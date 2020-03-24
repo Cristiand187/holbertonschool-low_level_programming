c6e6bbf44494d01e93d44957115e24c0edf1a52e
cristiand187 <cristiand187@hotmail.com>
2020-03-24 18:25:10 -0500

    0x13. C - More singly linked lists - Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (index != NULL)
 	{
-		if (index == 0)
+		if (idx == 0)
 		{
 			new->next = index->next;
 			*head = new;
