904d57215412ef1b0e47971938d9c96c624d450c
cristiand187 <cristiand187@hotmail.com>
2020-03-26 15:37:49 -0500

    0x14. C - Manipulación de bits - Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -29,6 +29,9 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int count = 0, decimal = 0, i = 0;
 
+	if (b == NULL)
+		return (0);
+
 	while (b[count] != '\0')
 	{
 		if (b[count] != '0' && b[count] != '1')
