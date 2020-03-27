8bb416910b96f78680d254d23ae0b08df56586b3
cristiand187 <cristiand187@hotmail.com>
2020-03-26 21:40:28 -0500

    0x14. C - Manipulación de bits - Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include "holberton.h"
+
+
+/**
+ * clear_bit - This function that converts a binary number to
+ * @n: decimal number.
+ * @index: index binary.
+ * Return: 1 or -1 value.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(int) * 8)
+		return (-1);
+
+	if (*n != 0)
+	{
+		*n = *n ^ (1 << index);
+		return (1);
+	}
+	return (-1);
+}
