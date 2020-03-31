2be61f84eeba8a54ab3bc354bb5aa2f1f3c6a0ae
Cristiand187 <Cristiand187@hotmail.com>
2020-03-31 18:13:05 -0500

    0x15. C - File I/O - Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd < 0)
 	{
-		return (0);
+		return (-1);
 	}
 
 	if (text_content == NULL)
@@ -40,7 +40,7 @@ int create_file(const char *filename, char *text_content)
 
 	close(fd);
 
-	if (len == -1 || len != num_bytes)
+	if (num_bytes < 0 || len != num_bytes)
 		return (-1);
 
 	return (1);
