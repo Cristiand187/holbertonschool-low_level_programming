542ce6ce499fa35a067a521d20c6efc5703bc2cf
Cristiand187 <Cristiand187@hotmail.com>
2020-03-31 16:30:11 -0500

    0x15. C - File I/O - Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,36 +14,40 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char buf[letters + 1];
-	size_t num_bytes = 0, i = 0;
+	int fd, i = 0;
+	char *buf;
+	size_t num_bytes = 0;
 
 	if (*filename == '\0')
 	{
 		return (0);
 	}
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY, 0644);
 
 	if (fd == -1)
 	{
 		return (0);
 	}
 
-	num_bytes = read(fd, buf, letters);
+	buf = malloc(sizeof(char) * letters);
 
-	close(fd);
+	if (buf == NULL)
+		return (0);
+
+	num_bytes = read(fd, buf, letters);
 
 	if (num_bytes < letters)
 	{
 		num_bytes += 1;
 	}
 
-	while (i < num_bytes)
-	{
-		_putchar(buf[i]);
-		i++;
-	}
+	write(STDOUT_FILENO, buf, num_bytes);
+
+	close(fd);
+
+	if (i == -1)
+		return (0);
 
 	return (num_bytes);
 }
