07f9a2fe43e3dfac192c8d29ff79d1dccfa3c1f7
cristiand187 <cristiand187@hotmail.com>
2020-05-29 13:28:39 +0000

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,21 +9,26 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_teble;
 	hash_node_t **new_node;
+	unsigned long int idx = 0;
 
-	new_hash_teble = malloc(sizeof(hash_table_t) * size);
+	new_hash_teble = malloc(sizeof(hash_table_t));
 	if (new_hash_teble == NULL)
 		return NULL;
 
 	new_hash_teble->size = size;
 
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = malloc(sizeof(hash_node_t) * 1024);
 	if (new_node == NULL)
 	{
 		free(new_hash_teble);
 		return NULL;
 	}
 	
-
+	while (idx < size)
+	{
+		new_node[idx] = NULL;
+		idx++;
+	}
 	new_hash_teble->array = new_node;
 
 	return (new_hash_teble);
