630933e9ba959700885ff48ce4dee58bac15ef40
cristiand187 <cristiand187@hotmail.com>
2020-05-30 00:35:15 +0000

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash_teble;
 	unsigned long int idx = 0;
 
+	if (size < 0)
+		return (NULL);
+
 	new_hash_teble = malloc(sizeof(hash_table_t));
 	if (new_hash_teble == NULL)
 		return (NULL);
