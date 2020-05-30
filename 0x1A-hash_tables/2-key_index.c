b8fced432a1eea4c6250e87a5f6b931b2c2bd979
cristiand187 <cristiand187@hotmail.com>
2020-05-30 00:00:58 +0000

    0x1A. C - Hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
- *
- * Return: 
+ * key_index - function that gives you the index of a key.
+ * @key: key is the key
+ * @size: the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
