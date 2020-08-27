a516dbe4ecab0b5daa6679f711281913538e5f72
Cristiand187 <cristiand187@hotmail.com>
2020-08-27 16:33:03 -0500

    0x1E. C - Search Algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+void selection_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void print_array(int *array, size_t l, size_t r);
 
 #endif /* SEARCH_ALGOS_H */
